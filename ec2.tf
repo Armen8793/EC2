bdd6b73dcb58458106c7cdfe49d358b2db4dd955
Armen8793 <80597213+Armen8793@users.noreply.github.com>
2025-03-18 08:50:05 +0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ubuntu_vm" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t3.medium"
-  key_name               = "armen"
+  key_name               = var.key
   subnet_id              = aws_subnet.public_subnet.id
   vpc_security_group_ids        = [aws_security_group.ubuntu_sg.id]
   associate_public_ip_address = true
