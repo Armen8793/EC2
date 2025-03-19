8fdcae1f525ac9ec51059e2d44f7266208193d4d
Armen <petrosyanarmen723@gmail.com>
2025-03-19 09:42:23 +0400

    fic

--- output.tf
+++ output.tf
@@ -1,3 +1,9 @@
-output "instance_ip" {
-  value = aws_instance.ubuntu_vm.public_ip
+resource "local_file" "inventory_file" {
+  content = templatefile("./inventory.template",
+    {
+      ec2_public_ip = aws_instance.ubuntu_vm.public_ip
+    }
+  )
+  filename = "./inventory"
 }
+
