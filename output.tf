a4431d1de75c8b89763190816bc7c7788b3a90d8
Armen <petrosyanarmen723@gmail.com>
2025-03-19 09:34:43 +0400

    fix

--- output.tf
+++ output.tf
@@ -1,8 +1,3 @@
-resource "local_file" "inventory_file" {
-  content = templatefile("./inventory.template",
-    {
-      ec2_public_ip = [aws_instance.ubuntu_vm.public_ip]
-    }
-  )
-  filename = "./inventory"
+output "instance_ip" {
+  value = aws_instance.ubuntu_vm.public_ip
 }
